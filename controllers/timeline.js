b2a753d0ba43c7db4f3d6ed3750be783672b5ed1
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-05-19 23:55:46 +0530

    cloudnary

--- controllers/timeline.js
+++ controllers/timeline.js
@@ -1,117 +1,74 @@
-import {PostModel} from '../models/timeline.js'
+import { PostModel } from '../models/timeline.js';
 
+// Get all posts
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error('Error retrieving posts:', error);
+    res.status(500).json({ error: 'Failed to retrieve posts' });
+  }
+};
 
-// export const create =  async (req, res) => {
-//     try {
-//       const { title, description, year } = req.body;
-//       const { filename, path } = req.file;
-  
-//       const post = new PostModel({
-//         image: path,
-//         title,
-//         description,
-//         year
-//       });
-  
-//       await post.save();
-  
-//       res.status(200).json({ message: 'Post created successfully' });
-//     } catch (error) {
-//       console.error('Error creating post:', error);
-//       res.status(500).json({ error: 'Failed to create post' });
-//     }
-//   };
-  
-  // Get all posts
-  export const getAll= async (req, res) => {
-    try {
-      const posts = await PostModel.find();
-      res.status(200).json(posts);
-    } catch (error) {
-      console.error('Error retrieving posts:', error);
-      res.status(500).json({ error: 'Failed to retrieve posts' });
-    }
-  };
-  
-
-
-
-  // Update a post by ID
-  export const updateTask =  async (req, res) => {
-    try {
-      const { title, description, year } = req.body;
-  
-      const post = await PostModel.findByIdAndUpdate(
-        req.params.id,
-        { title, description, year },
-        { new: true }
-      );
-  
-      if (!post) {
-        return res.status(404).json({ error: 'Post not found' });
-      }
-  
-      res.status(200).json(post);
-    } catch (error) {
-      console.error('Error updating post:', error);
-      res.status(500).json({ error: 'Failed to update post' });
-    }
-  };
-
+// Get a post by ID
 export const getmyPost = async (req, res) => {
   try {
-    // const year_id = req.params.id;
-
     const post = await PostModel.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
 
     res.status(200).json({
       success: true,
       post,
     });
   } catch (error) {
-    next(error);
+    console.error('Error retrieving post:', error);
+    res.status(500).json({ error: 'Failed to retrieve post' });
   }
 };
 
-   export const deletepost = async (req, res) => {
-    try {
-      const post = await PostModel.findById(req.params.id);
-      if (!post) {
-        return res.status(404).json({ error: 'Post not found' });
-      }
-      await post.deleteOne();
+// Update a post by ID
+export const updateMyPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const { title, description, year } = req.body;
 
-      res.status(200).json({ message: 'Post deleted successfully' });
-    } catch (error) {
-      console.error('Error deleting post:', error);
-      res.status(500).json({ error: 'Failed to delete post' });
-    }
-  };
+    const post = await PostModel.findById(postId);
 
-  export const updateMyPost = async (req, res) => {
-    try {
-      const postId = req.params.id;
-      const { title, description, year } = req.body;
-  
-      // Find the post by ID in the database
-      const post = await PostModel.findById(postId);
-  
-      if (!post) {
-        return res.status(404).json({ message: 'Post not found' });
-      }
-  
-      // Update the post fields
-      post.title = title;
-      post.description = description;
-      post.year = year;
-  
-      // Save the updated post
-      const updatedPost = await post.save();
-  
-      res.json(updatedPost);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Internal server error' });
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
     }
-  };
-  
+
+    post.title = title;
+    post.description = description;
+    post.year = year;
+
+    const updatedPost = await post.save();
+
+    res.json(updatedPost);
+  } catch (error) {
+    console.error('Error updating post:', error);
+    res.status(500).json({ error: 'Failed to update post' });
+  }
+};
+
+// Delete a post by ID
+export const deletepost = async (req, res) => {
+  try {
+    const post = await PostModel.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    await post.deleteOne();
+
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting post:', error);
+    res.status(500).json({ error: 'Failed to delete post' });
+  }
+};
--- routes/timeline.js
+++ routes/timeline.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { updateMyPost, deletepost, getAll, getmyPost, updateTask } from "../controllers/timeline.js";
+import { updateMyPost, deletepost, getAll, getmyPost  } from "../controllers/timeline.js";
 import multer from 'multer';
 import { PostModel } from "../models/timeline.js";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from 'url';
-
+import cloudinary from 'cloudinary';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = express.Router();
 
-const uploadDir = path.join(__dirname, 'uploads');
+const uploadDir = path.join(__dirname, '../uploads');
 
 // Create the multer storage configuration
 console.log("Upload directory:", uploadDir);
@@ -24,17 +24,26 @@ const storage = multer.diskStorage({
   }
 });
 
-// Create the multer upload instance
 const upload = multer({ storage });
+
+// Configure Cloudinary
+cloudinary.config({ 
+  cloud_name: 'dbqrdphkj', 
+  api_key: '425779795216949', 
+  api_secret: 'GYT6PdCVdxnPM_BJT1IzIsr02BI' 
+});
 
 router.post("/new", upload.single('image'), async (req, res) => {
   try {
     const { title, description, year } = req.body;
-    const image = req.file.path;
+    const imagePath = req.file.path; // Get the path of the uploaded file
+
+    // Upload the image to Cloudinary
+    const uploadedImage = await cloudinary.v2.uploader.upload(imagePath);
 
     // Create a new post
     const post = new PostModel({
-      image,
+      image: uploadedImage.secure_url, // Use the secure URL provided by Cloudinary
       title,
       description,
       year
@@ -45,6 +54,7 @@ router.post("/new", upload.single('image'), async (req, res) => {
 
     res.status(201).json(post);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Failed to create a new post" });
   }
 });
