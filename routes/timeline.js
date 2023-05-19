a88734d99d879573066c266070035654809df157
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-05-19 18:10:07 +0530

    again

--- routes/timeline.js
+++ routes/timeline.js
@@ -5,7 +5,7 @@ import { PostModel } from "../models/timeline.js";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from 'url';
-import path from 'path';q
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
